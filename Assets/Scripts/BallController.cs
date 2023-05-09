668194345fb68d35898b74d71b6f1a37e182c1ad
Hanif <85260485+hxnmi@users.noreply.github.com>
2023-05-09 11:25:52 +0700

    add speeder & dotween obstacle

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -3,91 +3,83 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class BallController : MonoBehaviour, IPointerDownHandler 
-{
-	[SerializeField] Collider col;
-	[SerializeField] Rigidbody rb;
-	[SerializeField] float force;
-	[SerializeField] LineRenderer aimLine;
-	[SerializeField] Transform aimWorld;
-	bool shoot;
-	bool shootingMode;
-	float forceFactor;
-	Vector3 forceDirection;
-	Ray ray;
-	Plane plane;
-	
-	public bool ShootingMode { get => shootingMode; }
-	// Update is called once per frame
-	void Update() {
-		if (shootingMode) {
-			if (Input.GetMouseButtonDown(0))
-			{
-				aimLine.gameObject.SetActive(true);
-				aimWorld.gameObject.SetActive(true);
-				plane = new Plane(Vector3.up, this.transform.position);
-			} 
-			else if (Input.GetMouseButton(0)) 
-			{
-				var moveViewportPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
-				var ballViewportPos = Camera.main.WorldToViewportPoint(this.transform.position);
-				var ballScreenPos = Camera.main.WorldToViewportPoint(this.transform.position);
-				var pointerDirection = ballViewportPos - moveViewportPos;
-				pointerDirection.z = 0;
+public class BallController : MonoBehaviour, IPointerDownHandler {
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float force;
+    [SerializeField] LineRenderer aimLine;
+    [SerializeField] Transform aimWorld;
+    bool shoot;
+    bool shootingMode = true;
+    float forceFactor;
+    Vector3 forceDirection;
+    Ray ray;
+    Plane plane;
 
-				/*
-				// draw aim
-				aimLine.transform.position = ballScreenPos;
-				var positions = new Vector3[] { ballScreenPos, Input.mousePosition };
-				aimLine.SetPositions(positions);
-				*/
+    public bool ShootingMode { get => shootingMode; }
 
-				// force direction
-				ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-				plane.Raycast(ray, out var distance);
-				forceDirection = this.transform.position - ray.GetPoint(distance);
-				forceDirection.Normalize();
+    // Update is called once per frame
+    void Update() {
+        if (shootingMode) {
+            if (Input.GetMouseButtonDown(0)) {
+                aimLine.gameObject.SetActive(true);
+                aimWorld.gameObject.SetActive(true);
+                plane = new Plane(Vector3.up, this.transform.position);
+            } else if (Input.GetMouseButton(0)) {
+                var mouseViewportPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+                var ballViewportPos = Camera.main.WorldToViewportPoint(this.transform.position);
+                var pointerDirection = ballViewportPos - mouseViewportPos;
+                pointerDirection.z = 0;
 
-				// force factor
-				forceFactor = pointerDirection.magnitude * 2;
+                // force direction
+                ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                plane.Raycast(ray, out var distance);
+                forceDirection = this.transform.position - ray.GetPoint(distance);
+                forceDirection.Normalize();
 
-				//aim visual
-				aimWorld.transform.position = this.transform.position;
-				aimWorld.forward = forceDirection;
-				aimWorld.localScale = new Vector3(1,1,0.5f + forceFactor);
+                // force factor
+                forceFactor = pointerDirection.magnitude * 2;
 
-			} 
-			else if (Input.GetMouseButtonUp(0)) 
-			{
-				shoot = true;
-				shootingMode = false;
-				aimLine.gameObject.SetActive(false);
-				aimWorld.gameObject.SetActive(false);
-			}
-		}
-	}
+                //aim visual
+                aimWorld.transform.position = this.transform.position;
+                aimWorld.forward = forceDirection;
+                aimWorld.localScale = new Vector3(1, 1, 0.5f + forceFactor);
 
-	private void FixedUpdate() {
-		if (shoot) {
-			shoot = false;
-			rb.AddForce(forceDirection * force * forceFactor, ForceMode.Impulse);
-		}
+                var ballScreenPos = Camera.main.WorldToScreenPoint(this.transform.position);
+                var mouseScreenPos = Input.mousePosition;
+                ballScreenPos.z = 1;
+                mouseScreenPos.z = 1;
+                var positions = new Vector3[]
+                {
+                    Camera.main.ScreenToWorldPoint(ballScreenPos),
+                    Camera.main.ScreenToWorldPoint(mouseScreenPos)
+                };
+                aimLine.SetPositions(positions);
+            } else if (Input.GetMouseButtonUp(0)) {
+                shoot = true;
+                shootingMode = false;
+                aimLine.gameObject.SetActive(false);
+                aimWorld.gameObject.SetActive(false);
+            }
+        }
+    }
 
-		if (rb.velocity.sqrMagnitude < 0.01f && rb.velocity.sqrMagnitude > -0.01f) {
-			rb.velocity = Vector3.zero;
-		}
-	}
+    private void FixedUpdate() {
+        if (shoot) {
+            shoot = false;
+            rb.AddForce(forceDirection * force * forceFactor, ForceMode.Impulse);
+        }
+        if (rb.velocity.sqrMagnitude < 0.01f && rb.velocity.sqrMagnitude != 0) {
+            rb.velocity = Vector3.zero;
+        }
+    }
 
-	public bool IsMove() {
-		return rb.velocity != Vector3.zero;
-	}
+    public bool IsMove() {
+        return rb.velocity != Vector3.zero;
+    }
 
-	void IPointerDownHandler.OnPointerDown(PointerEventData eventData) 
-	{
-		if (this.IsMove())
-			return;
-
-		shootingMode = true;
-	}
-
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData) {
+        if (this.IsMove())
+            return;
+        shootingMode = true;
+    }
 }
--- Assets/Scripts/PlayManager.cs
+++ Assets/Scripts/PlayManager.cs
@@ -2,64 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayManager : MonoBehaviour
-{
-	[SerializeField] BallController ballController;
-	[SerializeField] CameraController camController;
-	
-	private bool isBallOutside;
-	private bool isBallTeleporting;
-	private bool isGoal;
+public class PlayManager : MonoBehaviour {
+    [SerializeField] BallController ballController;
+    [SerializeField] CameraController camController;
 
-	private Vector3 lastBallPosition;
+    private bool isBallOutside;
+    private bool isBallTeleporting;
+    private bool isGoal;
 
-	// Update is called once per frame
-	void Update()
-	{
-		if (ballController.ShootingMode) 
-		{
-			lastBallPosition = ballController.transform.position;
-		}
+    private Vector3 lastBallPosition;
 
-		var inputActive = Input.GetMouseButton(0) 
-			&& ballController.IsMove() == false 
-			&& ballController.ShootingMode == false 
-			&& isBallOutside == false ;
-		camController.SetInputActive(inputActive);
-	}
-	
-	public void OnBallGoalEnter () {
+    // Update is called once per frame
+    void Update() {
 
-		isGoal = true;
-		ballController.enabled = false;
-		//TODO window win popup
-	}
+        if (ballController.ShootingMode) {
+            lastBallPosition = ballController.transform.position;
+        }
 
-	public void OnBallOutside () {
+        var inputActive = Input.GetMouseButton(0)
+            && ballController.IsMove() == false
+            && ballController.ShootingMode == false
+            && isBallOutside == false;
 
-		if (isGoal) {
-			return;
-		}
-		if (isBallTeleporting == false) {
-			Invoke("TeleportBallLastPosition", 3);
-		}
-		ballController.enabled = false;
-		isBallOutside = true;
-		isBallTeleporting = true;
-	}
+        camController.SetInputActive(inputActive);
+    }
 
-	public void TeleportBallLastPosition () {
-		TeleportBall(lastBallPosition);
-	}
+    public void OnBallGoalEnter() {
 
-	public void TeleportBall(Vector3 targetPosition) {
-		var rb = ballController.GetComponent<Rigidbody>();
-		rb.isKinematic = true;
-		ballController.transform.position = targetPosition;
-		rb.isKinematic = false;
+        isGoal = true;
+        ballController.enabled = false;
+        //TODO window win popup
+    }
 
-		ballController.enabled = true;
-		isBallOutside = false;
-		isBallTeleporting = false;
-	}
+    public void OnBallOutside() {
+
+        if (isGoal) {
+            return;
+        }
+        if (isBallTeleporting == false) {
+            Invoke("TeleportBallLastPosition", 3);
+        }
+        ballController.enabled = false;
+        isBallOutside = true;
+        isBallTeleporting = true;
+    }
+
+    public void TeleportBallLastPosition() {
+        TeleportBall(lastBallPosition);
+    }
+
+    public void TeleportBall(Vector3 targetPosition) {
+        var rb = ballController.GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+        ballController.transform.position = targetPosition;
+        rb.isKinematic = false;
+
+        ballController.enabled = true;
+        isBallOutside = false;
+        isBallTeleporting = false;
+    }
 }
